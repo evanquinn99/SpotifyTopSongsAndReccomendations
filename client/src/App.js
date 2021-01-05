25639da15e03bfcbcb115f6ed050594d6e979df6
equinn <equinn@emoneyadvisor.com>
2021-01-05 15:44:28 -0500

    Made getRecents a component

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 /*spotify web api library courtesy of Jose M Perez: https://github.com/jmperez/spotify-web-api-js */
 const spotifyApi = new SpotifyWebApi();
-class App extends Component {
 
+class NowPlaying extends Component {
   constructor() {
     /* this getHash code is coming from the spotify example */
     super();
@@ -43,10 +43,11 @@ class App extends Component {
         });
       })
   }
+
   
   render() {
     return (
-      <div className="App">
+      <div className="NowPlaying">
         <a href='http://localhost:8888' > Login to Spotify </a>
       <div>
         Now Playing: { this.state.nowPlaying.name }
@@ -59,9 +60,89 @@ class App extends Component {
           Check Now Playing
         </button>
       }
+
+      
     </div>
+    
     );
   }
 }
 
-export default App;
+export class GetRecents extends Component {
+
+  constructor() {
+    /* this getHash code is coming from the spotify example */
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+
+    this.state = {
+      loggedIn: token ? true : false,
+      size: 0
+    }
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getRecentTracks(){
+    let recentTracks = [];
+    spotifyApi.getMyRecentlyPlayedTracks({limit: 5})
+      .then((response)=>{
+        console.log(response);
+        for (let i =0 ; i < response.items.length; i++){
+          let song = {
+            title : response.items[i].track.name,
+            artist : response.items[i].track.artists[0].name,
+            image : response.items[i].track.album.images[0].url
+          }
+          recentTracks.push(song)
+        }
+        console.log(recentTracks);
+        this.setState({size: recentTracks.length});
+        return recentTracks
+      })
+  }
+
+  render() {
+    let rows = [];
+    for (var i = 0; i < this.state.size; i++){
+      let rowID = `row${i}`
+      let cell = []
+      for (var idx = 0; idx < 3; idx++){
+        let cellID = `cell${i}-${idx}`
+        cell.push(<td key={cellID} id={cellID}></td>)
+      }
+      rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+    }
+
+    return(
+      <div className = "GetRecents" >
+        <div>{
+         this.state.loggedIn && 
+          <button onClick={() => this.getRecentTracks()}>
+          check recents
+        </button> 
+      }</div>
+      </div>
+    )
+
+  }
+
+
+}
+
+export default NowPlaying;
+//export  GetRecents;
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {GetRecents} from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GetRecents />
   </React.StrictMode>,
   document.getElementById('root')
 );
